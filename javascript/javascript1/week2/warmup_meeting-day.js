3c7278cbcbb5b73778f38af2dfb1de265de66b0f
unknown <kseniiazar@gmail.com>
2018-12-15 16:29:45 +0100

    Homework week2 JS1

---
+++ javascript/javascript1/week2/warmup_meeting-day.js
@@ -0,0 +1,25 @@
+/* Task:
+Today is Sunday and you ask your friend in how many days you are meeting. 
+If the friend says I will see you in 9 days. The output should be Tuesday. 
+You should get the today's day from the system. So typical console output is:
+        Today is: Sunday
+        How many days to meet : 9
+        We are meeting on : Tuesday
+    Hint: use remainder operator */
+
+
+let week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+function meeting (dayFrom, inDays) {
+    for (let i=0; i < week.length; i++ ){
+        if (dayFrom === week[i]) {
+            let a = i + 1 + inDays;
+            let meetDay = week[a % 7 - 1];
+            console.log ("Today is: " + dayFrom);
+            console.log ("How many days to meet: " + inDays);
+            console.log ("We are meeting on: " + meetDay);
+        } else continue;
+    }
+}
+meeting ("Sunday", 9);
+
+
