79b0c21233fd4042099eec5210687aa94e0ba296
Kseniia Zar <kseniiazar@gmail.com>
2019-02-15 12:46:54 +0100

    Homework JS3 week3

--- javascript/javascript3/week3/art/main.js
+++ javascript/javascript3/week3/art/main.js
@@ -1,6 +1,9 @@
-let c = document.querySelector('#canvas'),
-      circle = c.getContext ('2d');
+let canvas = document.querySelector('#canvas'),
+    circle = canvas.getContext ('2d');
+    circle.canvas.width = window.innerWidth;
+    circle.canvas.height = window.innerHeight;
 
+    //circle template
 class Circle {
     constructor (x, y, r, startAngle, endAngle, fillColor) {
         this.x = x;
@@ -12,13 +15,51 @@ class Circle {
     }
 
     draw () {
+        circle.beginPath();
         circle.fillStyle = this.fillColor;
         circle.arc (this.x,this.y,this.r,this.startAngle, this.endAngle);
         circle.fill ();
+        
     }
 }
 
-const c1 = new Circle(50, 50, 20, 0, 2*Math.PI, '#FFC5D1');
-const c2 = new Circle(100, 50, 10, 0, 2*Math.PI, 'blue');
-c1.draw();
-c2.draw();
+//generating of random color
+const getRandomColor = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i=0; i<6; i++) {
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+//drawing circles
+setInterval (() => {
+    x = Math.floor (Math.random() * 1500);
+    y = Math.floor (Math.random() * 840);
+    r = Math.floor (Math.random() * 30);
+    fillColor = getRandomColor ();
+    let c = new Circle (x, y, r, 0, Math.floor(2*Math.PI), fillColor);
+    c.draw();
+}, 50)
+
+//creating circles depending on mouse location
+
+let mouseX = 0;
+let mouseY = 0;
+
+// track mouse position
+let mouseLocation = () => {
+    let e = window.event;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+canvas.addEventListener ('mousemove', () => {
+    mouseLocation ();
+    r = Math.floor (Math.random() * 30);
+    fillColor = getRandomColor ();
+    let c = new Circle (mouseX, mouseY, r, 0, Math.floor(2*Math.PI), fillColor);
+    c.draw();
+})
+
