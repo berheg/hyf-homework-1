f09ea27e0867ba5f569c3b9ed934b62b8215b355
Kseniia Zar <kseniiazar@gmail.com>
2019-02-17 12:43:05 +0100

    suggestion from the class

--- javascript/javascript3/week3/git_repos/main.js
+++ javascript/javascript3/week3/git_repos/main.js
@@ -1,32 +1,62 @@
-const user1 = fetch('https://api.github.com/search/repositories?q=user:keerthi1822')
-    .then ((response) => response.json())
-const user2 = fetch('https://api.github.com/search/repositories?q=user:Sanahayat')
-    .then ((response) => response.json())
-const user3 = fetch('https://api.github.com/search/repositories?q=user:nmoskaleva')
-    .then ((response) => response.json())
+// const user1 = fetch('https://api.github.com/search/repositories?q=user:keerthi1822')
+//     .then ((response) => response.json())
+// const user2 = fetch('https://api.github.com/search/repositories?q=user:Sanahayat')
+//     .then ((response) => response.json())
+// const user3 = fetch('https://api.github.com/search/repositories?q=user:nmoskaleva')
+//     .then ((response) => response.json())
 
-let usersUl = document.querySelector ('.users')
+// let usersUl = document.querySelector ('.users')
 
-Promise.all ([user1, user2, user3])
-    .then ((responses) => {
-        responses.map ((user) => {
-            let userLi = document.createElement ('li')
-            userLi.innerHTML = user.items[0].owner.login;
-            usersUl.appendChild (userLi);
+// Promise.all ([user1, user2, user3])
+//     .then ((responses) => {
+//         responses.map ((user) => {
+//             let userLi = document.createElement ('li')
+//             userLi.innerHTML = user.items[0].owner.login;
+//             usersUl.appendChild (userLi);
 
-            user.items.map((repo) => {
+//             user.items.map((repo) => {
                     
-                let newUl = document.createElement ('ul');
-                let li = document.createElement ('li');
-                li.innerHTML = repo.name + ': ' + repo.html_url;
+//                 let newUl = document.createElement ('ul');
+//                 let li = document.createElement ('li');
+//                 li.innerHTML = repo.name + ': ' + repo.html_url;
     
-                newUl.appendChild (li);
-                userLi.appendChild (newUl);
+//                 newUl.appendChild (li);
+//                 userLi.appendChild (newUl);
                 
-            });
-        })
+//             });
+//         })
         
         
         
-    })
+//     })
 
+    const githubUserRepositiries = ['https://api.github.com/search/repositories?q=user:keerthi1822','https://api.github.com/search/repositories?q=user:Sanahayat', 'https://api.github.com/search/repositories?q=user:nmoskaleva']
+    const repositoryPromises = githubUserRepositiries.map (repository => fetch(repository))
+    let usersUl = document.querySelector ('.users');
+
+    Promise.all(repositoryPromises)
+        .then (responses => {
+            const jsonPromises = responses.map(response => response.json());
+            //console.log(jsonPromises);
+
+            return Promise.all (jsonPromises)
+        })
+        .then(repositories => {
+            console.log(repositories);
+            repositories.map ((user) => {
+                let userLi = document.createElement ('li')
+                userLi.innerHTML = user.items[0].owner.login;
+                usersUl.appendChild (userLi);
+    
+                user.items.map((repo) => {
+                        
+                    let newUl = document.createElement ('ul');
+                    let li = document.createElement ('li');
+                    li.innerHTML = repo.name + ': ' + repo.html_url;
+        
+                    newUl.appendChild (li);
+                    userLi.appendChild (newUl);
+                    
+                });
+            })
+        });
